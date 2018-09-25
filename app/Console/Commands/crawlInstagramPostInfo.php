b39e4e9e825a5a0f309fc476540723f863b79027
k-ichikawa <chottokeinkana@gmail.com>
2018-09-25 18:52:54 +0900

    add curl option

--- app/Console/Commands/crawlInstagramPostInfo.php
+++ app/Console/Commands/crawlInstagramPostInfo.php
@@ -44,6 +44,7 @@ class crawlInstagramPostInfo extends Command
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $res = curl_exec($ch);
